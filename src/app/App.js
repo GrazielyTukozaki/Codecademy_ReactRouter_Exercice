fb29829e8afa8bd81509883416e9a5210d764cdc
GrazielyTukozaki <grazielybasilio@hotmail.com>
2022-06-18 15:41:03 -0300

    Adding Dynamic routes and redirect when the user try to acess profile page without log in

--- src/app/App.js
+++ src/app/App.js
@@ -8,7 +8,7 @@ import Article from "../components/Article";
 import Categories from "../components/Categories";
 import Author from "../components/Author";
 import Profile from "../components/Profile";
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
 import "./App.css";
@@ -18,27 +18,29 @@ function App() {
     <Router>
       <Header />
       <main>
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/sign-up'>
-          <SignUp />
-        </Route>
-        <Route path='/articles'>
-          <Articles />
-        </Route>
-        <Route path='/article'>
-          <Article />
-        </Route>
-        <Route path='/categories'>
-          <Categories />
-        </Route>
-        <Route path='/author'>
-          <Author />
-        </Route>
-        <Route path='/profile'>
-          <Profile />
-        </Route>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/sign-up'>
+            <SignUp />
+          </Route>
+          <Route path='/articles/:title'>
+            <Article />
+          </Route>
+          <Route path='/articles'>
+            <Articles />
+          </Route>
+          <Route path='/categories'>
+            <Categories />
+          </Route>
+          <Route path='/authors/:name'>
+            <Author />
+          </Route>
+          <Route path='/profile'>
+            <Profile />
+          </Route>
+        </Switch>
       </main>
       <Footer />
     </Router>
